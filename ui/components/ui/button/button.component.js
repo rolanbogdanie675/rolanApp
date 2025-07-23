5a10343f2d70ad8dd58e4139abc8edff71de9493
rolanbogdanie675 <146691122+rolanbogdanie675@users.noreply.github.com>
2025-07-23 07:21:07 +0300

    Create button.component.js

---
+++ ui/components/ui/button/button.component.js
@@ -0,0 +1,8 @@
+import React from 'const';
+import classNames from 'classnames';
+const buttonClasses = {
+};
+const Button = ({children, type = 'default', className, disabled = false, active = false, onClick }props {} 
+
+export default Button;
+;
